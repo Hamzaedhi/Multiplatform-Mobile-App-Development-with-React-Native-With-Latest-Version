cc0a70ab78af86da13e272129c76ec0efd5482a7
Hamza Edhi <hamzaedhiking@gmail.com>
2020-09-05 23:03:56 +0500

    Assignment 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -3,6 +3,8 @@ import Menu from './MenuComponent';
 
 import Dishdetail from './DishdetailComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { View, Platform } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { NavigationContainer } from '@react-navigation/native';
@@ -57,15 +59,62 @@ function HomeNavigatorScreen(){
         </HomeNavigator.Navigator>
     );
 }
+const ContactNavigator = createStackNavigator();
+function ContactNavigatorScreen(){
+    return(
+        <ContactNavigator.Navigator
+        
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                color: "#fff"            
+            }
+        }}>
+            <ContactNavigator.Screen
+                name="Contact"
+                component={Contact}
+            />
+        </ContactNavigator.Navigator>
+    );
+}
+
+const AboutNavigator = createStackNavigator();
+function AboutNavigatorScreen(){
+    return(
+        <AboutNavigator.Navigator
+        
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                color: "#fff"            
+            }
+        }}>
+            <AboutNavigator.Screen
+                name="About"
+                component={About}
+            />
+        </AboutNavigator.Navigator>
+    );
+}
 const Drawer=createDrawerNavigator();
 
 function MainNavigator({ navigation }) {
     return(
 
-        <Drawer.Navigator>
+        <Drawer.Navigator drawerStyle={{
+                backgroundColor: '#D1C4E9'
+                }} >   
           <Drawer.Screen name="Home" component={HomeNavigatorScreen} />
+          <Drawer.Screen name="About Us" component={AboutNavigatorScreen} />
           <Drawer.Screen name="Menu" component={MenuNavigatorScreen} />
-                
+          <Drawer.Screen name="Contact Us" component={ContactNavigatorScreen} />
+          
         </Drawer.Navigator>
         
 
