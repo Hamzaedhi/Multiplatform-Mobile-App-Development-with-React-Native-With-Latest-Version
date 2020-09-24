1bdada0169d7cc3c0322fbc0813ec77e65d7f82c
Hamza Edhi <hamzaedhiking@gmail.com>
2020-09-24 14:18:59 +0500

    Network Info

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { ScrollView, Text, View, Image, StyleSheet } from 'react-native';
+import { View, Platform, Text, ScrollView, Image, StyleSheet, ToastAndroid } from 'react-native';
+import NetInfo from "@react-native-community/netinfo";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer';
@@ -27,6 +28,8 @@ const mapDispatchToProps = dispatch => ({
   fetchPromos: () => dispatch(fetchPromos()),
   fetchLeaders: () => dispatch(fetchLeaders()),
 })
+
+
 
 const MenuNavigator = createStackNavigator();
 
@@ -386,7 +389,37 @@ class Main extends Component {
     this.props.fetchComments();
     this.props.fetchPromos();
     this.props.fetchLeaders();
-  }    
+    NetInfo.fetch().then((connectionInfo) => {
+              ToastAndroid.show('Initial Network Connectivity Type: '
+                  + connectionInfo.type, ToastAndroid.LONG)
+          });
+          
+          NetInfo.addEventListener(connectionChange => this.handleConnectivityChange(connectionChange))
+      }
+    
+  componentWillUnmount() {
+    NetInfo.removeEventListener('connectionChange', this.handleConnectivityChange);
+  }
+  
+  handleConnectivityChange = (connectionInfo) => {
+    switch (connectionInfo.type) {
+      case 'none':
+        ToastAndroid.show('You are now offline!', ToastAndroid.LONG);
+        break;
+      case 'wifi':
+        ToastAndroid.show('You are now connected to WiFi!', ToastAndroid.LONG);
+        break;
+      case 'cellular':
+        ToastAndroid.show('You are now connected to Cellular!', ToastAndroid.LONG);
+        break;
+      case 'unknown':
+        ToastAndroid.show('You now have unknown connection!', ToastAndroid.LONG);
+        break;
+      default:
+        break;
+    }
+  }
+    
 
   render() {
  
